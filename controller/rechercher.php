fd42463013962a1418d61050ae1e3279eded4d56
Robin Paitry <paitrydev@gmail.com>
2020-01-06 11:43:04 +0100

    Create rechercher.php

---
+++ controller/rechercher.php
@@ -0,0 +1,10 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/projet2/cinematheca/common/connexionBase.php');
+// Selection dans la base de donnée des éléments à ressortir pour la recherche
+    $champRecherche = '%'.$_POST['cherche'].'%';
+    $stmt = $connection->prepare("SELECT id, titre, url_affiche FROM film WHERE titre like :titre");
+    $stmt->bindParam(':titre', $champRecherche);
+    $stmt->execute();
+    $filmCherche = $stmt->fetchall();
+// Encodage json pour le récupérer en javascript
+    echo json_encode($filmCherche);
